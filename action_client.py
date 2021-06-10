0e87481ef68b1f4a02895c737611dd0c25ce33c4
arincbulgur <37184019+arincbulgur@users.noreply.github.com>
2021-06-10 18:44:23 -0500

    Update action_client.py

--- action_client.py
+++ action_client.py
@@ -4,27 +4,31 @@ import actionlib
 from arl_nav_msgs.msg import GotoRegionAction, GotoRegionGoal
 
 if __name__ == '__main__':
+    #Some initializations depending on map
+    grid_size = 2.5
+    fg_x_offset = -177.75       #Offset values for floodedground environment
+    fg_y_offset = -496.75
+    init_x_offset = -300         #Offset values for simulation initialization
+    init_y_offset = 0
+    #Initialize client node and connect it to the action server
     rospy.init_node('goto_region_client')
     print("Waiting for server")
     client = actionlib.SimpleActionClient('warty/goto_region', GotoRegionAction)
     client.wait_for_server()
     print("Found server.")
 
+    #Read the input trajectory text file
     policy = open("traj_uPOMDP-uncertain-1.txt", 'r')
 
+    #Extract x-y coordinates for the waypoints line by line
     for index, line in enumerate(policy):
-        if index < 5 or index%10!=0:
-            continue
-        #if index > 1:
-        #    continue
         line_split = line.split(" ")
         x = int(line_split[0].strip('(, '))
         y = int(line_split[1].strip(')\n'))
-        next_x = -198.75 + x*2.5 + 21 - 310 + 8
-        next_y = -473.75 + y*2.5 - 23 + 53 + 1
-        print(next_x)
-        print(next_y)
+        next_x = x*grid_size + fg_x_offset + init_x_offset
+        next_y = y*grid_size + fg_y_offset + init_y_offset
 
+        #Setting up the goal message
         goal_msg = GotoRegionGoal()
         goal_msg.region_center.header.stamp = rospy.Time.now()
         goal_msg.region_center.pose.position.x = -next_y
@@ -32,10 +36,12 @@ if __name__ == '__main__':
         goal_msg.region_center.pose.orientation.w = 1
         goal_msg.radius = 1.8
         goal_msg.angle_threshold = 1.47
-        #goal_msg.region_center.header.frame_id = "world"
         goal_msg.region_center.header.frame_id = "warty/map"
+        
+        #Sending the message to the server
         client.send_goal(goal_msg)
-        #client.wait_for_result(rospy.Duration.from_sec(5.0))
+        
+        #Check for result
         client.wait_for_result()
         result = client.get_result()
         print(result)
